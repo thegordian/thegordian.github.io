19fc92ae203a8e7add070018a634fe463a414604
esaqib <73029692+esaqib@users.noreply.github.com>
2020-11-01 01:56:39 +0500

    updated gorian website

--- updated_website/js/scripts.js
+++ updated_website/js/scripts.js
@@ -81,3 +81,8 @@ function on_resize(width, height) {
         $(".team-text-box").addClass("position-absolute");
     }
 }
+$('.modal').on("hidden.bs.modal", function (e) { //fire on closing modal box
+    if ($('.modal:visible').length) { // check whether parent modal is opend after child modal close
+        $('body').addClass('modal-open'); // if open mean length is 1 then add a bootstrap css class to body of the page
+    }
+});
