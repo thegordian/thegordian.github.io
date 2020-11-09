343268c3b9274a52bdfa2388acfce0298b54d510
esaqib <73029692+esaqib@users.noreply.github.com>
2020-11-09 19:17:28 +0500

    putting on root

---
+++ assets/mail/contact_me.js
@@ -0,0 +1,91 @@
+$(function () {
+    $(
+        "#contactForm input, #contactForm select,#contactForm textarea,#contactForm button"
+    ).jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function ($form, event, errors) {
+            event.preventDefault();
+            // additional error messages or events
+        },
+        submitSuccess: function ($form, event) {
+            event.preventDefault(); // prevent default submit behaviour
+            // get values from FORM
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var subject = $("select#subject").val();
+            debugger;
+            var phone = $("input#phone").val();
+            var message = $("textarea#message").val();
+            var firstName = name; // For Success/Failure Message
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(" ") >= 0) {
+                firstName = name.split(" ").slice(0, -1).join(" ");
+            }
+            $this = $("#sendMessageButton");
+            $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+            $.ajax({
+                url: "https://spatialstack.com/gordian_php/gordian_email_sender.php",
+                // url: "gordian_php/gordian_email_sender.php", // local url
+                type: "POST",
+                data: {
+                    name: name,
+                    email: email,
+                    subject: subject,
+                    phone: phone,
+                    message: message,
+                },
+                cache: false,
+                success: function () {
+                    // Success message
+                    $("#success").html("<div class='alert alert-success'>");
+                    $("#success > .alert-success")
+                        .html(
+                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                        )
+                        .append("</button>");
+                    $("#success > .alert-success").append(
+                        "<strong>Your message has been sent. </strong>"
+                    );
+                    $("#success > .alert-success").append("</div>");
+                },
+                error: function () {
+                    // Fail message
+                    $("#success").html("<div class='alert alert-danger'>");
+                    $("#success > .alert-danger")
+                        .html(
+                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                        )
+                        .append("</button>");
+                    $("#success > .alert-danger").append(
+                        $("<strong>").text(
+                            "Sorry " +
+                                firstName +
+                                ", it seems that my mail server is not responding. Please try again later!"
+                        )
+                    );
+                    $("#success > .alert-danger").append("</div>");
+                },
+                complete: function () {
+                    setTimeout(function () {
+                        $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+                    }, 1000);
+                    //clear all fields
+                    $("#contactForm").trigger("reset");
+                },
+            });
+        },
+        filter: function () {
+            return $(this).is(":visible");
+        },
+    });
+
+    $('a[data-toggle="tab"]').click(function (e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+/*When clicking on Full hide fail/success boxes */
+$("#name").focus(function () {
+    $("#success").html("");
+});
---
+++ js/scripts.js
@@ -0,0 +1,104 @@
+/*!
+    * Start Bootstrap - Agency v6.0.2 (https://startbootstrap.com/template-overviews/agency)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+    */
+(function ($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+            this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length
+                ? target
+                : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate(
+                    {
+                        scrollTop: target.offset().top - 72,
+                    },
+                    1000,
+                    "easeInOutExpo"
+                );
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#mainNav",
+        offset: 74,
+    });
+
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+    on_resize($(window).width(), $(window).height());
+    $('#team-slider').lightSlider({
+        item: 1,
+        controls: false,
+        pager: false,
+        slideMargin: 0,
+        auto: true,
+        loop: true
+    });
+})(jQuery); // End of use strict
+
+
+$(window).on('resize', function () {
+    var win = $(this); //this = window
+    on_resize(win.width(), win.height());
+});
+function on_resize(width, height) {
+    // setTimeout(function() {
+    //     $(".team-text-box").css("left", $(".team-box > img").width());
+    // }, 1500);
+    if (width <= 1100) {
+        $(".team-box > img").removeClass("position-absolute");
+        $(".team-text-box").removeClass("position-absolute");
+    } else {
+        $(".team-box > img").addClass("position-absolute");
+        $(".team-text-box").addClass("position-absolute");
+    }
+}
+$('.modal').on("hidden.bs.modal", function (e) { //fire on closing modal box
+    if ($('.modal:visible').length) { // check whether parent modal is opend after child modal close
+        $('body').addClass('modal-open'); // if open mean length is 1 then add a bootstrap css class to body of the page
+    }
+});
+
+$(document).on("click", ".contact_more_info_btn", function (e) {
+    scrollToId("contact");
+    var subject = $(this).attr("data-subject");
+    $("#subject").val(subject);
+});
+
+function scrollToId(id) {
+    $("html, body").animate(
+        {
+            scrollTop: $("#" + id).offset().top - 72,
+        },
+        1000,
+        "easeInOutExpo"
+    );
+}
